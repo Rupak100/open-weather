91c83c3525c7616e93e36900c3e52d2d3c0c76fd
Rupak100 <112826831+Rupak100@users.noreply.github.com>
2024-11-16 13:57:54 +0530

    initial commit

---
+++ openWeather.js
@@ -0,0 +1,17 @@
+async function fetchWeatherData(latitude, longitude) {
+  const apiKey = "my_api";
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Error fetching data: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    localStorage.setItem("weatherData", JSON.stringify(data));
+    console.log("Weather data saved to localStorage.");
+  } catch (error) {
+    console.error("Error fetching weather data:");
+  }
+}
